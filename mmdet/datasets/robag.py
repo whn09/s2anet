6b9640ed02a84744a90ed23e0d751ce8bf0011dc
Ubuntu <ubuntu@ip-172-31-58-31.ec2.internal>
2021-02-01 06:56:25 +0000

    change annotation

--- mmdet/datasets/robag.py
+++ mmdet/datasets/robag.py
@@ -8,6 +8,10 @@ import xml.etree.ElementTree as ET
 import mmcv
 import numpy as np
 
+from DOTA_devkit.ResultMerge_multi_process import mergebypoly
+from DOTA_devkit.dota_evaluation_task1 import voc_eval
+from mmdet.core import rotated_box_to_poly_single
+
 @DATASETS.register_module
 class RobagDataset(XMLDataset):
 
@@ -37,16 +41,13 @@ class RobagDataset(XMLDataset):
             name = obj.find('name').text
             label = self.cat2label[name]
             difficult = int(obj.find('difficult').text)
-            bnd_box = obj.find('bndbox')
+            bnd_box = obj.find('robndbox')
             bbox = [
-                int(bnd_box.find('x0').text),
-                int(bnd_box.find('y0').text),
-                int(bnd_box.find('x1').text),
-                int(bnd_box.find('y1').text),
-                int(bnd_box.find('x2').text),
-                int(bnd_box.find('y2').text),
-                int(bnd_box.find('x3').text),
-                int(bnd_box.find('y3').text)
+                float(bnd_box.find('cx').text),
+                float(bnd_box.find('cy').text),
+                float(bnd_box.find('w').text),
+                float(bnd_box.find('h').text),
+                float(bnd_box.find('angle').text)
             ]
             ignore = False
             if self.min_size:
@@ -62,13 +63,13 @@ class RobagDataset(XMLDataset):
                 bboxes.append(bbox)
                 labels.append(label)
         if not bboxes:
-            bboxes = np.zeros((0, 8))
+            bboxes = np.zeros((0, 5))
             labels = np.zeros((0, ))
         else:
             bboxes = np.array(bboxes, ndmin=2) - 1
             labels = np.array(labels)
         if not bboxes_ignore:
-            bboxes_ignore = np.zeros((0, 8))
+            bboxes_ignore = np.zeros((0, 5))
             labels_ignore = np.zeros((0, ))
         else:
             bboxes_ignore = np.array(bboxes_ignore, ndmin=2) - 1
@@ -79,3 +80,66 @@ class RobagDataset(XMLDataset):
             bboxes_ignore=bboxes_ignore.astype(np.float32),
             labels_ignore=labels_ignore.astype(np.int64))
         return ann
+
+    def evaluate(self, results, work_dir=None, gt_dir=None, imagesetfile=None):
+        dst_raw_path = osp.join(work_dir, 'results_before_nms')
+        dst_merge_path = osp.join(work_dir, 'results_after_nms')
+        mmcv.mkdir_or_exist(dst_raw_path)
+        mmcv.mkdir_or_exist(dst_merge_path)
+
+        print('Saving results to {}'.format(dst_raw_path))
+        self.result_to_txt(results, dst_raw_path)
+
+        print('Merge results to {}'.format(dst_merge_path))
+        mergebypoly(dst_raw_path, dst_merge_path)
+
+        print('Start evaluation')
+        detpath = osp.join(dst_merge_path, '{:s}.txt')
+        annopath = osp.join(gt_dir, '{:s}.txt')
+
+        classaps = []
+        map = 0
+        for classname in self.CLASSES:
+            rec, prec, ap = voc_eval(detpath,
+                                     annopath,
+                                     imagesetfile,
+                                     classname,
+                                     ovthresh=0.5,
+                                     use_07_metric=True)
+            map = map + ap
+            print(classname, ': ', ap)
+            classaps.append(ap)
+
+        map = map / len(self.CLASSES)
+        print('map:', map)
+        classaps = 100 * np.array(classaps)
+        print('classaps: ', classaps)
+        # Saving results to disk
+        with open(osp.join(work_dir, 'eval_results.txt'), 'w') as f:
+            res_str = 'mAP:' + str(map) + '\n'
+            res_str += 'classaps: ' + ' '.join([str(x) for x in classaps])
+            f.write(res_str)
+        return map
+
+    def result_to_txt(self, results, results_path):
+        img_names = [img_info['filename'] for img_info in self.img_infos]
+
+        assert len(results) == len(img_names), 'len(results) != len(img_names)'
+
+        for classname in self.CLASSES:
+            f_out = open(osp.join(results_path, classname + '.txt'), 'w')
+            print(classname + '.txt')
+            # per result represent one image
+            for img_id, result in enumerate(results):
+                for class_id, bboxes in enumerate(result):
+                    if self.CLASSES[class_id] != classname:
+                        continue
+                    if bboxes.size != 0:
+                        for bbox in bboxes:
+                            score = bbox[5]
+                            bbox = rotated_box_to_poly_single(bbox[:5])
+                            temp_txt = '{} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n'.format(
+                                osp.splitext(img_names[img_id])[0], score, bbox[0], bbox[1], bbox[2], bbox[3], bbox[4],
+                                bbox[5], bbox[6], bbox[7])
+                            f_out.write(temp_txt)
+            f_out.close()
